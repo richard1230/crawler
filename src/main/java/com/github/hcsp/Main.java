19dda20c72702e9c580f3adcf17dcd56506c78f1
richard1230 <huyongjinnuaa@163.com>
2020-02-06 22:49:08 +0800

    Edit checkstyle
    src/main/java/com/github/hcsp/Main.java:[48,47]
    (whitespace) WhitespaceAfter: ',' is not followed
     by whitespace

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -1,4 +1,5 @@
 package com.github.hcsp;
+
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -8,8 +9,10 @@ import org.apache.http.util.EntityUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+
 import java.io.IOException;
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) throws IOException {
 
@@ -20,56 +23,56 @@ public class Main {
         Set<String> processedLinks = new HashSet<>();
         linkpool.add("https://sina.cn/");
 
-        while (true){
-            if (linkpool.isEmpty()){
-               break;
+        while (true) {
+            if (linkpool.isEmpty()) {
+                break;
             }
             //Arraylist从尾部删除更有效率
             //remove会返回删除的那个元素,故这里不需要使用get函数
-            String link = linkpool.remove(linkpool.size()-1);
+            String link = linkpool.remove(linkpool.size() - 1);
 
             //如果处理过了，什么都不做
-            if (processedLinks.contains(link)){
+            if (processedLinks.contains(link)) {
                 continue;
             }
             //这里的\\/里面的第一个符号为转移符号
-            if (link.contains("sina.cn") && !link.contains("passport.sina.cn") && link.contains("news.sina.cn") && !link.contains("\\/")|| "https://sina.cn/".equals(link) ) {
+            if (link.contains("sina.cn") && !link.contains("passport.sina.cn") && link.contains("news.sina.cn") && !link.contains("\\/") || "https://sina.cn/".equals(link)) {
 
                 //这是我们感兴趣的,我们只处理新浪站内的链接
                 //就是拿到它的数据
                 CloseableHttpClient httpclient = HttpClients.createDefault();
 
                 System.out.println(link);
-                if (link.startsWith("//")){
+                if (link.startsWith("//")) {
                     link = "https:" + link;
                 }
 
                 HttpGet httpGet = new HttpGet(link);
-                httpGet.addHeader("User-Agent","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36");
+                httpGet.addHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36");
 
                 try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
                     System.out.println(response1.getStatusLine());
                     HttpEntity entity1 = response1.getEntity();
-                     String html = EntityUtils.toString(entity1);
+                    String html = EntityUtils.toString(entity1);
 
                     Document doc = Jsoup.parse(html);
                     //select：css选择器
-                   ArrayList<Element> links  = doc.select("a");
+                    ArrayList<Element> links = doc.select("a");
 
-                   for (Element aTag: links){
-                      linkpool.add(aTag.attr("href"));
+                    for (Element aTag : links) {
+                        linkpool.add(aTag.attr("href"));
 
-                   }
+                    }
 
-                   //假如这是一个新闻的详情页面,就存入数据库,否则,就什么都不做
+                    //假如这是一个新闻的详情页面,就存入数据库,否则,就什么都不做
                     ArrayList<Element> articleTags = doc.select("article");
-                    if (!articleTags.isEmpty()){
-                        for (Element articleTag:articleTags){
+                    if (!articleTags.isEmpty()) {
+                        for (Element articleTag : articleTags) {
                             String title = articleTags.get(0).child(0).text();
                             System.out.println(title);
                         }
                     }
-                        processedLinks.add(link);
+                    processedLinks.add(link);
                 }
             } else {
                 //这是我们不敢兴趣的,不处理它
@@ -78,7 +81,6 @@ public class Main {
 
 
         }
-
 
 
     }
