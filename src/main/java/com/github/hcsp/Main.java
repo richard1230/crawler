e3576d42c4d5aebc0e5a52583b47b0cdeb7fb271
richard1230 <huyongjinnuaa@163.com>
2020-02-08 11:20:59 +0800

    refactor some codes about eight functions are built for refacfoting

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -36,52 +36,76 @@ public class Main {
                 continue;
             }
             //这里的\\/里面的第一个符号为转移符号
-            if (link.contains("sina.cn") && !link.contains("passport.sina.cn") && link.contains("news.sina.cn") && !link.contains("\\/") || "https://sina.cn/".equals(link)) {
+            if (isInterestingLink(link)) {
 
-                //这是我们感兴趣的,我们只处理新浪站内的链接
-                //就是拿到它的数据
-                CloseableHttpClient httpclient = HttpClients.createDefault();
+                Document doc = httpGetAndParseHtml(link);
+                doc.select("a").stream().map(aTag->aTag.attr("href")).forEach(linkpool::add);
 
-                System.out.println(link);
-                if (link.startsWith("//")) {
-                    link = "https:" + link;
-                }
-
-                HttpGet httpGet = new HttpGet(link);
-                httpGet.addHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36");
-
-                try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
-                    System.out.println(response1.getStatusLine());
-                    HttpEntity entity1 = response1.getEntity();
-                    String html = EntityUtils.toString(entity1);
-
-                    Document doc = Jsoup.parse(html);
-                    //select：css选择器
-                    ArrayList<Element> links = doc.select("a");
-
-                    for (Element aTag : links) {
-                        linkpool.add(aTag.attr("href"));
-
-                    }
-
-                    //假如这是一个新闻的详情页面,就存入数据库,否则,就什么都不做
-                    ArrayList<Element> articleTags = doc.select("article");
-                    if (!articleTags.isEmpty()) {
-                        for (Element articleTag : articleTags) {
-                            String title = articleTags.get(0).child(0).text();
-                            System.out.println(title);
-                        }
-                    }
-                    processedLinks.add(link);
-                }
+                //假如这是一个新闻的详情页面,就存入数据库,否则,就什么都不做
+                //有注释的地方就可以会被重构
+                StoreIntoDatabaseIfItisNewsPage(doc);
+                processedLinks.add(link);
             } else {
                 //这是我们不敢兴趣的,不处理它
                 continue;
             }
+        }
+    }
 
+    private static void StoreIntoDatabaseIfItisNewsPage(Document doc) {
+        ArrayList<Element> articleTags = doc.select("article");
+        if (!articleTags.isEmpty()) {
+            for (Element articleTag : articleTags) {
+                String title = articleTags.get(0).child(0).text();
+                System.out.println(title);
+            }
+        }
+    }
+
+    private static Document httpGetAndParseHtml(String link) {
+
+        //这是我们感兴趣的,我们只处理新浪站内的链接
+        //就是拿到它的数据
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        System.out.println(link);
+        if (link.startsWith("//")) {
+            link = "https:" + link;
+        }
+        HttpGet httpGet = new HttpGet(link);
+        httpGet.addHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36");
+        try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
+            System.out.println(response1.getStatusLine());
+            HttpEntity entity1 = response1.getEntity();
+            String html = EntityUtils.toString(entity1);
+            return Jsoup.parse(html);
+            //select：css选择器
 
         }
+    }
 
+    private static boolean isInterestingLink(String link) {
+        return IsHomePage(link)&& IsNewsPage(link) && IsillegalString(link) || IsIndexPage(link)
+                &&  IsNotLoginPage(link);
+    }
+
+    private static boolean IsHomePage(String link) {
+        return link.contains("sina.cn");
+    }
+
+    private static boolean IsIndexPage(String link) {
+        return "https://sina.cn/".equals(link);
+    }
+
+    private static boolean IsillegalString(String link) {
+        return !link.contains("\\/");
+    }
+
+    private static boolean IsNewsPage(String link) {
+        return link.contains("news.sina.cn");
+    }
+
+    private static boolean IsNotLoginPage(String link) {
+        return !link.contains("passport.sina.cn") ;
 
     }
 }
