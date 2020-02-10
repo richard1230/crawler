ca1f9fa70e72b633e574cd0f855eaef522fbdc86
richard1230 <huyongjinnuaa@163.com>
2020-02-10 09:20:40 +0800

    Finish Database Operation before orm

---
+++ src/main/resources/db/migration/V1__Create_tables.sql
@@ -0,0 +1,17 @@
+create table NEWS(
+id bigint primary key auto_increment,
+title text,
+content text,
+url varchar(1000),
+created_at timestamp,
+modified_at timestamp
+                 );
+
+create table links_to_be_processed(
+    link varchar(1000)
+);
+
+create table LINKS_ALREADY_PROCESSED(
+    link varchar(1000)
+);
+-- select * from LINKS_TO_BE_PROCESSED;
