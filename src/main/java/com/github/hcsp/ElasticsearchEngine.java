3106e997e30d1663a7ee1534fc5e6458ba97323f
richard1230 <huyongjinnuaa@163.com>
2020-02-13 16:22:56 +0800

    es-billion-level-operation a simple search engine is created

--- src/main/java/com/github/hcsp/ElasticsearchEngine.java
+++ src/main/java/com/github/hcsp/ElasticsearchEngine.java
@@ -8,13 +8,14 @@ import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.index.query.MultiMatchQueryBuilder;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class ElasticsearchEngine {
     public static void main(String[] args) throws IOException {
-        while (true){
+        while (true) {
             System.out.println("please input a search keyword:");
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
             String keyword = reader.readLine();
@@ -26,10 +27,10 @@ public class ElasticsearchEngine {
     private static void search(String keyword) throws IOException {
         try (RestHighLevelClient client = new RestHighLevelClient(RestClient.builder(new HttpHost("localhost", 9200, "http")))) {
             SearchRequest request = new SearchRequest("news");
-            request.source(new SearchSourceBuilder().query(new MultiMatchQueryBuilder(keyword,"title","content")));
+            request.source(new SearchSourceBuilder().query(new MultiMatchQueryBuilder(keyword, "title", "content")));
             SearchResponse result = client.search(request, RequestOptions.DEFAULT);
 
-            result.getHits().forEach(hit-> System.out.println(hit.getSourceAsString()));
+            result.getHits().forEach(hit -> System.out.println(hit.getSourceAsString()));
 
         }
     }
